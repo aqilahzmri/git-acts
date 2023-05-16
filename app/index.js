1c4bacb919d7e6a6bfec7782e65b7b433b93a7f3
aqilahmsrh <127908263+aqilahmsrh@users.noreply.github.com>
2023-05-16 09:47:51 +0800

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("AWS Lambda and S3 Bucket - Helloooooo ")
+    body: JSON.stringify("AWS Lambda and S3 Bucket - Helloooooo How Are You")
   };
   return response;
 }
